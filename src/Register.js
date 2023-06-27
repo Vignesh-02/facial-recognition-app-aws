c8115f01a45c50c92ef2dea5c45aa81359db0a60
Vignesh-02 <laserlikefocus000@gmail.com>
2023-06-27 15:38:39 +0530

    added John_Doe

--- src/Register.js
+++ src/Register.js
@@ -44,6 +44,7 @@ const Register = () => {
                 <input type='file' name='image' onChange={e => setImage(e.target.files[0])}></input>
                 <button type='submit'>Upload</button>
             </form>
+            <h2> Make sure to name your image as 'John_Doe.jpeg' </h2>
             <div className={isAuth ? 'success' : 'failure'}>{uploadResultMessage}</div>
             
     </div>
